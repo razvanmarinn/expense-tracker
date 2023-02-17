8a3485f3a26a99e2f079cc53351e2d4bb8b4f43c
razvanmarinn <r8activated@gmail.com>
2023-02-18 00:33:28 +0200

    made predicting next month spending to work

--- general/next_month_spending.py
+++ general/next_month_spending.py
@@ -1,31 +1,34 @@
 """Predicts the next month's spending based on the previous 3 months' spending."""
-import pandas as pd
 from abc import ABC, abstractmethod
-from statsmodels.tsa.arima_model import ARIMA
-from general.util import make_api_get_request
+from statistics import mean
+from statsmodels.tsa.arima.model import ARIMA
 from general.headers import headers, base_url
+from general.util import make_api_get_request
 
 
 class Predicter(ABC):
     """Abstract class for all predictors."""
     @abstractmethod
-    def predict(user_id):
+    def predict(self, user_id):
         """Predicts the next month's spending based on the previous 3 months' spending."""
-        pass
 
 
 class NextMonthPredicter(Predicter):
     """Predicts the next month's spending based on the previous 3 months' spending."""
-    def __init__(self, month, year):
-        self.month = month
+    def __init__(self, year):
         self.year = year
 
+    def make_api_call_for_month(self, user_id):
+        endpoint_url = f"{base_url}/users/get_spending_by_year/{user_id}/{self.year}"
+        return make_api_get_request(endpoint_url, headers=headers)
+
     def predict(self, user_id):
-        make_api_get_request(f"{base_url}/users/get_spending_by_month/{user_id}/{self.month}/{self.year}", headers=headers)
-        data = pd.read_csv('spending_data.csv', index_col='Date', parse_dates=True)
-        last_three_months = data.tail(3)
-        baseline_spending = last_three_months.mean()
-        model = ARIMA(data, order=(1, 1, 1))
-        model_fit = model.fit(disp=0)
-        prediction = model_fit.forecast(steps=1)[0]
+        """Predicts the next month's spending based on the previous 3 months' spending."""
+        months = self.make_api_call_for_month(user_id=user_id)
+        baseline_spending = mean(months)
+        model = ARIMA(months, order=(1, 1, 1))
+        model_fit = model.fit()
+        prediction = model_fit.forecast(steps=1, exog=None)[0]
+
         predicted_spending = prediction + baseline_spending  # noqa: F841
+        return predicted_spending
